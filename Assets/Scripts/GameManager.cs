1de1ab948e040936a42a26b2293943ecf41d3fc0
blackBoyCode <e96e6e6@gmail.com>
2019-09-17 01:07:00 -0400

    first iteration of GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,43 @@
-﻿using System.Collections;
+﻿/**
+ *                                    GAME_MANAGER TASK
+ * 
+- [x]  Present end screen (enable end screen canvas) with breakdown of players round
+    - [x]  how many bugs and what kind (Color bugs for now)
+    - [x]  what level played
+    - [x]  display player stats and the amount they are increasing for next round
+- [x]  Give instructions to player manager to upgrade player stats
+- [x]  Handle game reset with level change in mind
+    - [x]  level change would look like GM giving the instruction to SceneManager to change to "x" level
+ */
+
+/**
+ *
+ * 
+ * - Grey pick ups rewards the player with 10 points per unit
+ * - Orange pick ups rewards the player with 5 points + 10% eating speed per unit
+ * - Blue pick ups rewards the player with 5 points + 10% running speed per unit
+ * 
+ * at the moment "running speed" is walking speed *2, so we could either increase walking speed by 10% 
+ * or have running speed be its own defined variable.
+ */
+
+
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class GameManager : MonoBehaviour
 {
+    
     public float levelTime = 20;
 
     public bool isGameOver = false;
-    private bool scorePresented = false;
+    private bool scorePresented = false;//??
 
     public int 
             gameDeathCount,
@@ -31,7 +59,7 @@ public class GameManager : MonoBehaviour
 
     private void Update()
     {
-        if (isGameOver && !scorePresented)
+        if (isGameOver && !scorePresented) 
         {
             playerManager.FreezePlayer();
 
@@ -40,26 +68,46 @@ public class GameManager : MonoBehaviour
     }
 
     
-    private void DisplayScore()
+    public void DisplayScore()
     {
-        scorePresented = true;
+        scorePresented = true;//?
 
         playerStats.text = "Test Stats: "+ "\n"
-                           + "level: " + currentLevel +"\n"
-                           + "nextG: " + playerManager.greyPickUps + "\n" 
-                           + "nextO: " + playerManager.orangePickUps + "\n" 
-                           + "nextB: " + playerManager.bluePickUps;
-        
-        //todo: 
+                           + "Level: " + currentLevel +"\n"
+                           + "GreyPickUp: " + playerManager.greyPickUps + "\n" 
+                           + "OrangePickUp: " + playerManager.orangePickUps + "\n" 
+                           + "BluePickUp: " + playerManager.bluePickUps + "\n"
+                           + "PlayerTotalPoints: " + playerManager.playerPoints + "\n"
+                           + "PlayerEatSpeed: " + playerManager.eatSpeed + "\n"
+                           + "PlayerRunSpeed: " + playerManager.pRunSpeed + "\n";
+                
 
-        // tally & present player pick-up
     }
 
-    private void upgradePlayerStats( PlayerManager pm ) //TODO: why are the Stats to be updated?
+    
+    public void greyUpgrade( PlayerManager pm ) //may need better naming..
     {
-        //pm.
+        pm.playerPoints += 10;
+    }    
+    public void orangeUpgrade( PlayerManager pm )
+    {
+        pm.playerPoints += 5;
+        pm.eatSpeed += pm.eatSpeed * 0.10f; //increase by 10%
+    }  
+    
+    public void blueUpgrade( PlayerManager pm )
+    {
+        pm.playerPoints += 5;
+        pm.pRunSpeed +=  pm.pRunSpeed * 0.10f; //increase by 10%
     }
 
+    //change the level by calling game manager 
+    public void changeLevel(int selectedLevel)
+    {
+        SceneManager.LoadScene(selectedLevel);
+    }
+
+   
 
 
 }
