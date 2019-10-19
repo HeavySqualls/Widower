652d3f138f7ad3c07b3b983d90988be8a1fc7a49
AUniqueTim <43281258+AUniqueTim@users.noreply.github.com>
2019-10-19 00:05:54 -0400

    Menus working, but a bit buggy. Level pretty much done, just need a few more bugs placed. Added several predators. Added OOW/respawn gameobject to art scene.

---
+++ Assets/Scripts/OOWRespawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OOWRespawn : MonoBehaviour
+{
+    public Vector3 respawnPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player1" || other.tag == "Player2")
+        {
+            other.transform.position = respawnPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Menu loading...");
+    }
+    public void LoadSettingsMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("SettingsMenu");
+        Debug.Log("Settings menu loading...");
+    }
+    public void LoadControlsMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("ControlsMenu");
+        Debug.Log("Controls menu loading...");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Player quit game...");
+    }
+}
